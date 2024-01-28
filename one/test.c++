01640b5aaf9cfb10789d5547f7048d5842e9aeb2
mhd0012 <mahdi@mahdi.com>
2024-01-28 21:03:38 +0330

    test-one

---
+++ one/test.c++
@@ -0,0 +1,17 @@
+#include <iostream> 
+using namespace std;
+// this is a command 
+
+
+
+int main() {
+  int x, y;
+  int sum;
+  cout << "Type a number: ";
+  cin >> x;
+  cout << "Type another number: ";
+  cin >> y;
+  sum = x + y;
+  cout << "Sum is: " << sum;
+  return 0;
+}
---
+++ one/test2.cpp
@@ -0,0 +1,136 @@
+// int : Integer ------> 1 , 2 , 10 , 1000 , 1567465
+// str : string -----> salam man mahdi hastam .
+// float : Floating ------> 9.2 , 452.5155
+//  char : Character -------> D , z , F
+// bool : Boolean -------> True , False
+
+
+
+#include <iostream>
+// #include <string>
+using namespace std;
+
+
+// int x = 5 ;
+// int y = 10;
+// char z = 'Z' ;
+// int r = 656 ;string name = "Hello";  bool are_you_ok = true;
+
+// jsdhfgh kjsd
+// در اینجا داریم تابع تعریف میکنیم ...
+
+
+// int g = 55;
+// int main() {
+//   int a, f = 15 ,j = 8 ;
+//   a = 5;
+//   int sum = a + f + j ;
+//   cout << "Sum is : " << sum ;
+// }
+
+
+// int main () {
+//   // Creating variables
+//   int myNum = 5;               // Integer (whole number)
+//   float myFloatNum = 5.99;     // Floating point number
+//   double myDoubleNum = 9.98;   // Floating point number
+//   char myLetter = 'D';         // Character
+//   bool myBoolean = true;       // Boolean
+//   string myString = "Hello";   // String
+
+// //   ''
+   
+//   // Print variable values
+//   cout << "int: مسیتمنلتمنسیتمنسیبل  و ;;;;;;;;;;;;; " << myNum << "\n";
+//   cout << "float: " << myFloatNum << "\n";
+//   cout << "double: " << myDoubleNum << "\n\n\n";
+//   cout << "char: " << myLetter << "\t";
+//   cout << "bool: " << myBoolean << "\"";
+//   cout << "string: " << myString << "\n";
+ 
+//   return 0;
+// }
+
+
+
+
+// int main(){
+//   string name;
+//   cout << "Hi , what is your name ?! ";
+//   cin >> name ;
+//   cout << "your name is : " << name << "\n" ;
+// }
+
+
+
+
+// int main() {
+//   int x, y;
+//   int sum;
+//   cout << "Type a number: ";
+//   cin >> x;
+//   cout << "Type another number: ";
+//   cin >> y;
+//   sum = x + y;
+//   cout << "sum is: " << sum << "\n";
+// }
+
+
+// int main(){
+//   int a = 5 ;
+//   int b,c,d,e;
+//   b = a++ ;
+//   cout << "a is : " << a << "\n";
+//   cout << "b is : " << b << "\n\n";
+  
+//   c = ++a ;
+//   cout << "a is : " << a << "\n";
+//   cout << "c is : " << c << "\n\n";
+//   d = a--;
+//   cout << "a is : " << a << "\n";
+//   cout << "d is : " << d << "\n\n";
+//   e = --a ;
+//   cout << "a is : " << a << "\n";
+//   cout << "e is : " << e << "\n\n";
+// }
+
+
+
+
+// int main () {
+//     int number1 ;
+//     int number2;
+//     cout << "type number 1 : ";
+//     cin >> number1;
+//     cout << "type number 2 : ";
+//     cin >> number2;
+//     if(number1 == number2 )
+//         cout << number1 << " == " << number2 << "\n";
+//     if(number1 >= number2 )
+//         cout << number1 << " >= " << number2 << "\n" ;
+//     if(number1 <= number2 )
+//         cout << number1 << " <= " << number2 ;
+//     if(number1 != number2 )
+//         cout << number1 << " != " << number2 ;
+// }
+
+
+
+
+
+// int 12number = 5 ; 
+int number12 = 5 ;
+
+string myName = "Mahdi ;;;;;;;;;";
+
+
+
+
+
+
+
+
+
+
+
+
